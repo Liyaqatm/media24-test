b121e03f7c2b7b7e8ba5e691ec146d675df8021b
Mugjenli <Mugjenli@local>
2016-04-05 00:07:07 +0200

    Added Tests

---
+++ AcmeBank/test/com/acme/test01/liyaqat/TestChequeAccount.java
@@ -0,0 +1,12 @@
+package com.acme.test01.liyaqat;
+
+import org.junit.*;
+
+
+public class TestChequeAccount {
+
+	@Test
+	public void testBasicChequeAccount() {
+		System.out.println("Hello");
+	}
+}
---
+++ AcmeBank/test/com/acme/test01/liyaqat/TestSavingsAccount.java
@@ -0,0 +1,5 @@
+package com.acme.test01.liyaqat;
+
+public class TestSavingsAccount {
+
+}
